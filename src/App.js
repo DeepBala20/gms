fdcc44a727916f5e9e8d92f401c2e10335d30608
Deep_Bala <deepahir3105@gmail.com>
2023-12-30 12:15:33 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Profile from './pages/Profile'
 import Sign_out from './pages/Sign_out'
 import Sign_in from './pages/Sign_in'
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-500'>
+      <h1>
+        <span className="">Bala's</span>
+        <span>Grocery</span>
+      </h1>
+    </header>
+  )
+}
