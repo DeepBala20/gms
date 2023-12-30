a76aa9cbbe0773d35f1fa72454ca7bfb7907d351
Deep_Bala <deepahir3105@gmail.com>
2023-12-30 12:54:21 +0530

    header with styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <header className='bg-slate-500'>
-      <h1>
-        <span className="">Bala's</span>
-        <span>Grocery</span>
+    <header className='bg-teal-900'>
+      <div className='flex justify-between items-center mx-auto px-10 py-3'>
+      <Link to='/'>
+      <h1 className='flex flex-wrap font-bold items-baseline'>
+        <span className="text-slate-300 text-4xl">Bala's   </span>
+        <span className='text-slate-100 text-2xl'> Grocery</span>
       </h1>
+      </Link>
+      <ul className='flex gap-5 justify-between'>
+        <Link to='/'>
+          <li className='hover:underline text-slate-100'>Home</li>
+        </Link>
+        <Link to='/about'>
+          <li className='hover:underline text-slate-100'>About</li>
+        </Link>
+      </ul>
+      <ul>
+        <Link to='/profile'>
+          <li className='hover:underline text-slate-100'>Profile</li>
+        </Link>
+      </ul>
+      </div>
     </header>
   )
 }
